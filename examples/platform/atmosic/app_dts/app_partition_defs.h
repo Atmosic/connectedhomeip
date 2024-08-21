172470d4719b7d8cd28cf69db579f5bf4fbfba46
chu <chu@atmosic.com>
2024-08-20 23:51:04 -0700

    [SOFTWARE-6783] [Zephyr] Perth support atm_settings for matter apps
    Change-Id: I4a60cf621e176a556207a0e243a77c11e25b6220

---
+++ examples/platform/atmosic/app_dts/app_partition_defs.h
@@ -0,0 +1,25 @@
+/**
+ *******************************************************************************
+ *
+ * @file app_partition_defs.h
+ *
+ * @brief Atmosic application partition definitions, only to be included by
+ * device tree files as DTS overlays for partition layouts.
+ *
+ * Copyright (C) Atmosic 2024
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ *******************************************************************************
+ */
+
+#ifndef _ATMOSIC_ATM_APP_PARTITION_DEFS_H_
+#define _ATMOSIC_ATM_APP_PARTITION_DEFS_H_
+
+#ifndef ATM_STORAGE_SIZE
+#define ATM_STORAGE_SIZE 0x4000
+#else
+#error "ATM_STORAGE_SIZE redefinition conflict occurs"
+#endif
+
+#endif // _ATMOSIC_ATM_APP_PARTITION_DEFS_H_
