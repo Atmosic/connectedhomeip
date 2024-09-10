d2e4778c5a4fe1030de0f5d56aa744f50ceedecc
Txanton Bejos <tbejos@atmosic.com>
2024-09-10 20:53:55 +0000

    [SW-5875] Update for Zephyr 3.7.0
    *) Use the new Kconfig options for Zephyr 3.7.0 C++ builds
    *) Use newly added Zephyr recvmsg
    
    Change-Id: If3c096ad3653700b34080ccba3793b869e6bfa22

--- src/inet/ZephyrSocket.h
+++ src/inet/ZephyrSocket.h
@@ -30,24 +30,3 @@
 #if CHIP_SYSTEM_CONFIG_USE_ZEPHYR_SOCKETS
 #include <zephyr/net/socket.h>
 #endif
-
-static inline ssize_t recvmsg(int sock, struct msghdr * msg, int flags)
-{
-    // Zephyr doesn't implement recvmsg at all, but if the message vector size is > 0 we can simply
-    // translate recvmsg to recvfrom which fills only the first of the provided buffers (although
-    // we don't get control messages in such a case).
-
-    if (msg->msg_iovlen < 1)
-    {
-        errno = EMSGSIZE;
-        return -1;
-    }
-
-    ssize_t ret = recvfrom(sock, msg->msg_iov[0].iov_base, msg->msg_iov[0].iov_len, flags, static_cast<sockaddr *>(msg->msg_name),
-                           &msg->msg_namelen);
-
-    if (ret >= 0)
-        msg->msg_controllen = 0;
-
-    return ret;
-}
