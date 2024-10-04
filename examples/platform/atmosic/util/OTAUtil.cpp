7ad47232e539fcf383c28b28fad3c884c250d770
Derek Taubert <dtaubert@atmosic.com>
2024-10-04 14:21:50 -0700

    SOFTWARE-6815 Implement Matter OTA processor
    -) Mostly follow telink code for ATM34 OTA
    
    Change-Id: Ic7d4b6dd7470888cc2409923a03f825793990405

--- examples/platform/atmosic/util/OTAUtil.cpp
+++ examples/platform/atmosic/util/OTAUtil.cpp
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2022-2023 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,23 +15,23 @@
  *    limitations under the License.
  */
 
-#include "OTAUtil.h"
-
-#if CONFIG_CHIP_OTA_REQUESTOR
+#ifdef CONFIG_CHIP_OTA_REQUESTOR
 #include <app/clusters/ota-requestor/BDXDownloader.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestor.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorDriver.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorStorage.h>
-#include <app/server/Server.h>
-#include <platform/CHIPDeviceLayer.h>
-#include <platform/nrfconnect/OTAImageProcessorImpl.h>
-#include <zephyr/dfu/mcuboot.h>
-#endif
+#include <platform/atmosic/atm34/OTAImageProcessorImpl.h>
 
 using namespace chip;
 using namespace chip::DeviceLayer;
+#endif
 
-#if CONFIG_CHIP_OTA_REQUESTOR
+#include <zephyr/dfu/mcuboot.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(app, CONFIG_CHIP_APP_LOG_LEVEL);
+
+#ifdef CONFIG_CHIP_OTA_REQUESTOR
 
 namespace {
 
@@ -39,63 +39,35 @@ DefaultOTARequestorStorage sOTARequestorStorage;
 DefaultOTARequestorDriver sOTARequestorDriver;
 chip::BDXDownloader sBDXDownloader;
 chip::DefaultOTARequestor sOTARequestor;
+chip::DeviceLayer::OTAImageProcessorImpl sOTAImageProcessor;
 } // namespace
-
-// compile-time factory method
-OTAImageProcessorImpl & GetOTAImageProcessor()
-{
-#if CONFIG_PM_DEVICE && CONFIG_NORDIC_QSPI_NOR
-    static OTAImageProcessorImpl sOTAImageProcessor(&GetFlashHandler());
-#else
-    static OTAImageProcessorImpl sOTAImageProcessor;
-#endif
-    return sOTAImageProcessor;
-}
 
 void InitBasicOTARequestor()
 {
     VerifyOrReturn(GetRequestorInstance() == nullptr);
 
-    OTAImageProcessorImpl & imageProcessor = GetOTAImageProcessor();
-    imageProcessor.SetOTADownloader(&sBDXDownloader);
-    sBDXDownloader.SetImageProcessorDelegate(&imageProcessor);
+    sOTAImageProcessor.SetOTADownloader(&sBDXDownloader);
+    sBDXDownloader.SetImageProcessorDelegate(&sOTAImageProcessor);
     sOTARequestorStorage.Init(Server::GetInstance().GetPersistentStorage());
     sOTARequestor.Init(Server::GetInstance(), sOTARequestorStorage, sOTARequestorDriver, sBDXDownloader);
     chip::SetRequestorInstance(&sOTARequestor);
-    sOTARequestorDriver.Init(&sOTARequestor, &imageProcessor);
-    imageProcessor.TriggerFlashAction(ExternalFlashManager::Action::SLEEP);
+    sOTARequestorDriver.Init(&sOTARequestor, &sOTAImageProcessor);
 }
+
+#endif
 
 void OtaConfirmNewImage()
 {
-#ifndef CONFIG_SOC_SERIES_NRF53X
-    /* Check if the image is run in the REVERT mode and eventually
-    confirm it to prevent reverting on the next boot.
-    On nRF53 target there is not way to verify current swap type
-    because we use permanent swap so we can skip it. */
-    VerifyOrReturn(mcuboot_swap_type() == BOOT_SWAP_TYPE_REVERT);
-#endif
-
-    OTAImageProcessorImpl & imageProcessor = GetOTAImageProcessor();
-    if (!boot_is_img_confirmed())
+    if (mcuboot_swap_type() == BOOT_SWAP_TYPE_REVERT)
     {
-        CHIP_ERROR err = System::MapErrorZephyr(boot_write_img_confirmed());
-        if (CHIP_NO_ERROR == err)
+        int img_confirmation = boot_write_img_confirmed();
+        if (img_confirmation)
         {
-            imageProcessor.SetImageConfirmed();
-            ChipLogProgress(SoftwareUpdate, "New firmware image confirmed");
+            LOG_ERR("Image not confirmed %d. Will be reverted!", img_confirmation);
         }
         else
         {
-            ChipLogError(SoftwareUpdate, "Failed to confirm firmware image, it will be reverted on the next boot");
+            LOG_INF("Image confirmed");
         }
     }
-}
-
-#endif
-
-ExternalFlashManager & GetFlashHandler()
-{
-    static ExternalFlashManager sFlashHandler;
-    return sFlashHandler;
 }
--- examples/platform/atmosic/util/include/OTAUtil.h
+++ examples/platform/atmosic/util/include/OTAUtil.h
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2022-2023 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,26 +17,14 @@
 
 #pragma once
 
-#include <platform/nrfconnect/ExternalFlashManager.h>
-
-#if CONFIG_CHIP_OTA_REQUESTOR
-#include <platform/nrfconnect/OTAImageProcessorImpl.h>
+#ifdef CONFIG_CHIP_OTA_REQUESTOR
+#include <platform/atmosic/atm34/OTAImageProcessorImpl.h>
 
 namespace chip {
 namespace DeviceLayer {
 class OTAImageProcessorImpl;
 } // namespace DeviceLayer
 } // namespace chip
-
-/**
- * Select recommended OTA image processor implementation.
- *
- * If the application uses QSPI external flash and enables API for controlling
- * power states of peripherals, select the implementation that automatically
- * powers off the external flash when no longer needed. Otherwise, select the
- * most basic implementation.
- */
-chip::DeviceLayer::OTAImageProcessorImpl & GetOTAImageProcessor();
 
 /** Initialize basic OTA requestor.
  *
@@ -46,22 +34,9 @@ chip::DeviceLayer::OTAImageProcessorImpl & GetOTAImageProcessor();
  */
 void InitBasicOTARequestor();
 
-/**
- * Check if the current image is the first boot the after OTA update and if so
- * confirm it in MCUBoot.
- *
- * @return CHIP_NO_ERROR if the image has been confirmed, or it is not the first
- * boot after the OTA update.
- * Other CHIP_ERROR codes if the image could not be confirmed.
- */
-void OtaConfirmNewImage();
-
 #endif // CONFIG_CHIP_OTA_REQUESTOR
 
 /**
- * Get ExternalFlashManager static instance.
- *
- * Returned object can be used to control the QSPI external flash,
- * which can be introduced into sleep mode and woken up on demand.
+ * Confirm or revert new image in MCUBoot.
  */
-chip::DeviceLayer::ExternalFlashManager & GetFlashHandler();
+void OtaConfirmNewImage();
